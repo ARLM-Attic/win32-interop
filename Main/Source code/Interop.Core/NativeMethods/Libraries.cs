b34d6859d13d76a11c392c0e148e91f2b6222f04
afsherman_cp <SND\afsherman_cp>
2013-12-09 15:08:25 -0800

    - Fixed error in dll names in Libraries.cs

--- Main/Source code/Interop.Core/NativeMethods/Libraries.cs
+++ Main/Source code/Interop.Core/NativeMethods/Libraries.cs
@@ -7,9 +7,9 @@ namespace Interop.Core
         [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
         public static class ExternDll
         {
-            public const string Kernel = "kernel32.dll";
+            public const string Kernel32 = "kernel32.dll";
             public const string User32 = "user32.dll";
-            public const string GDI32 = "user32.dll";
+            public const string GDI32 = "gdi32.dll";
             public const string DWMAPI = "dwmapi.dll";
             public const string Shell32 = "shell32.dll";
         }
