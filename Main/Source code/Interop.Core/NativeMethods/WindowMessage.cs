b5d939387072d362f1ace7af970c727afef5e7a7
afsherman_cp <SND\afsherman_cp>
2013-12-09 15:17:49 -0800

    - ScrollBoxMessage moved to separate file

--- Main/Source code/Interop.Core/NativeMethods/WindowMessage.cs
+++ Main/Source code/Interop.Core/NativeMethods/WindowMessage.cs
@@ -9,34 +9,6 @@ namespace Interop.Core
     public static partial class NativeMethods
     {
         [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
-        public enum ScrollBoxMessage
-        {
-            SBM_SETPOS = 0x00E0,
-
-            SBM_GETPOS = 0x00E1,
-
-            SBM_SETRANGE = 0x00E2,
-
-            SBM_GETRANGE = 0x00E3,
-
-            SBM_ENABLE_ARROWS = 0x00E4,
-
-            //// Not defined 0x00E5
-
-            SBM_SETRANGEREDRAW = 0x00E6,
-
-            //// Not defined 0x00E7
-
-            //// Not defined 0x00E8
-
-            SBM_SETSCROLLINFO = 0x00E9,
-
-            SBM_GETSCROLLINFO = 0x00EA,
-
-            SBM_GETSCROLLBARINFO = 0x00EB,
-        }
-
-        [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
         public enum WindowMessage
         {
             WM_NULL = 0x0000,
